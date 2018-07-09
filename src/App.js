94432ad22eea30e44b25ea151cc0b83a246f4f81
crosten <crosten@LONMBP016.local>
2018-07-09 22:46:52 +0100

    chp3-manipulating-state

--- src/App.js
+++ src/App.js
@@ -5,6 +5,27 @@ import Person from './Person/Person';
 
 
 class App extends Component {
+
+    // Managed from inside the component, only available when extending component
+    state = {
+        persons: [
+            {name: 'Olwen', age: 28},
+            {name: 'Stefan', age: 29},
+        ],
+        otherData: SomeOtherObj
+    }
+
+
+    /* Protects from global scope of this */
+    switchNameHandler = () => {
+        console.log("was] clicked!");
+        // ~DOnt do this!! this.state.persons[0].name = "Soooomething";
+        this.setState({persons: [
+                {name: 'Olwen Shannon', age: 29},
+                {name: 'Stefan Cross', age: 30},
+            ]})
+    }
+
     render() {
         return (
             <div className="App">
@@ -15,8 +36,12 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
-                <Person name="Stefan" age="27"/>
-                <Person name="Olwen" age="27"/>
+                {/* note not switchNameHandler with out (), else would execute on render */}
+                <button onClick={this.switchNameHandler}>Switch name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+                    Foods: Salads, Vegetables
+                </Person>
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 
+
+// Props are passed in
 const person = (props) => {
-    return <p>I'm {props.name} and I am {props.age} years of youth!</p>
+    return (
+        <div>
+            <p>I'm {props.name} and I am {props.age} years of youth!</p>
+            <p>{props.children}</p>
+        </div>
+    )
 };
 
 export default person;
