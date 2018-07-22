4e1b1917d845a77c62940707a82a6d54e8fc32d0
crosten <crosten@LONMBP016.local>
2018-07-22 22:37:39 +0100

    chp7-converting-to-stateful-components

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Person.css';
 
-// Props are passed in
-const person = (props) => {
-    return (
-        <div className={classes.Person} >
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years of youth!</p>
-            <p>{props.children}</p>
-            {/* the value now adds two way binding, console will error for other field without onChange added */}
-            <input type="text" onChange={props.changed} value={props.name}></input>
-        </div>
-    )
+class Person extends Component {
+    render() {
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years of youth!</p>
+                <p>{this.props.children}</p>
+                {/* the value now adds two way binding, console will error for other field without onChange added */}
+                <input type="text" onChange={this.props.changed} value={this.props.name}></input>
+            </div>
+        )
+    }
 };
 
-export default person;
+export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Person from './Person/Person';
 
-const persons = (props) => props.persons.map((person, index) => {
-    return <Person
-            click={ () => props.clicked(index) }
-            name={person.name}
-            age={person.age}
-            // Key is used to enable react to efficiently read DOM and map changes from future to past state
-            key={person.id}
-            changed={(event) => props.changed(event, person.id)}
-        />
-    });
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person, index) => {
+            return <Person
+                click={ () => this.props.clicked(index) }
+                name={person.name}
+                age={person.age}
+                // Key is used to enable react to efficiently read DOM and map changes from future to past state
+                key={person.id}
+                changed={(event) => this.props.changed(event, person.id)}
+            />
+        });
 
-export default persons;
+    }
+}
+export default Persons;
